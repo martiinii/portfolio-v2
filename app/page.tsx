5729fd96f419663ce51ae02f8d107b3c30d6d7a3
Martini <me.martiinii@gmail.com>
2023-06-29 19:06:13 +0200

    Added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/footer'
 import { Hero } from '@/components/hero'
 import { MyProjects } from '@/components/projectsList'
 
@@ -6,6 +7,7 @@ export default function Home() {
         <>
             <Hero />
             <MyProjects />
+            <Footer />
         </>
     )
 }
---
+++ components/footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="text-center mx-5 mt-20 mb-10 tracking-tight">
+            <span>Designed and built by Martin in 2023</span>
+        </footer>
+    )
+}
