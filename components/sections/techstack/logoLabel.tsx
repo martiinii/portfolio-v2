6685a393e85701ae83baa86fc122a8dbde97f335
Martiinii <me.martiinii@gmail.com>
2023-07-02 00:56:03 +0200

    changed to forwardRef

--- components/sections/techstack/logoLabel.tsx
+++ components/sections/techstack/logoLabel.tsx
@@ -1,12 +1,20 @@
+import clsx from "clsx"
+import { HTMLAttributes, forwardRef } from "react"
+
 type LogoLabelProps = {
     icon: React.ReactNode,
     children: string
-}
-export const LogoLabel = ({ icon, children }: LogoLabelProps) => {
+} & HTMLAttributes<HTMLSpanElement>
+
+const LogoLabel = forwardRef<HTMLSpanElement, LogoLabelProps>(({ icon, children, className, ...props }, ref) => {
     return (
-        <span className="flex items-center gap-4">
+        <span className={clsx("flex items-center gap-4", className)} ref={ref} {...props}>
             {icon}
             {children}
         </span>
     )
-}
+})
+
+LogoLabel.displayName = 'Logo label';
+
+export { LogoLabel }
