38ab0e0f902957daeb8b680b225fe01b4889a176
Martiinii <me.martiinii@gmail.com>
2023-07-02 00:56:31 +0200

    using reusable component

--- components/sections/techstack/index.tsx
+++ components/sections/techstack/index.tsx
@@ -6,6 +6,7 @@ import { ChakraUILogo } from "@/components/icons/chakraui"
 import { TailwindLogo } from "@/components/icons/tailwind"
 import { ShadcnUILogo } from "@/components/icons/shadcnui"
 import { TypeScriptLogo } from "@/components/icons/typescript"
+import { AccordionTech } from "./accordionTech"
 
 export const TechStack = () => {
     return (
@@ -17,59 +18,47 @@ export const TechStack = () => {
                 collapsible
                 className="max-w-xl mx-auto"
             >
-                <AccordionItem value="TypeScript">
-                    <AccordionTrigger>
-                        <LogoLabel icon={<TypeScriptLogo />}>TypeScript</LogoLabel>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Adds additional syntax to JavaScript. With types we can catch errors as early as in editor to waste no time bug-hunting. Converts to many targets so it can run anywhere JavaScript runs
-                    </AccordionContent>
-                </AccordionItem>
+                <AccordionTech
+                    icon={TypeScriptLogo}
+                    label="TypeScript"
+                >
+                    Adds additional syntax to JavaScript. With types we can catch errors as early as in editor to waste no time bug-hunting. Converts to many targets so it can run anywhere JavaScript runs
+                </AccordionTech>
 
-                <AccordionItem value="React">
-                    <AccordionTrigger>
-                        <LogoLabel icon={<ReactLogo />}>React</LogoLabel>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        One of the most popular libraries for building front end web applications. It has big community that creates additional libraries to make web better
-                    </AccordionContent>
-                </AccordionItem>
+                <AccordionTech
+                    icon={ReactLogo}
+                    label="React"
+                >
+                    One of the most popular libraries for building front end web applications. It has big community that creates additional libraries to make web better
+                </AccordionTech>
 
-                <AccordionItem value="Next.js">
-                    <AccordionTrigger>
-                        <LogoLabel icon={<NextLogo />}>Next.js</LogoLabel>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Framework for building full-stack webapps that comes with latest features and many useful tools out of the box. It provides excellent performance and fast build times
-                    </AccordionContent>
-                </AccordionItem>
+                <AccordionTech
+                    icon={NextLogo}
+                    label="Next.js"
+                >
+                    Framework for building full-stack webapps that comes with latest features and many useful tools out of the box. It provides excellent performance and fast build times
+                </AccordionTech>
 
-                <AccordionItem value="Chakra UI">
-                    <AccordionTrigger>
-                        <LogoLabel icon={<ChakraUILogo />}>Chakra UI</LogoLabel>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Simple and easy to use component library with fantastic DX. Provides many components that are easily customizable, but lacks support for React Server Components
-                    </AccordionContent>
-                </AccordionItem>
+                <AccordionTech
+                    icon={ChakraUILogo}
+                    label="Chakra UI"
+                >
+                    Simple and easy to use component library with fantastic DX. Provides many components that are easily customizable, but lacks support for React Server Components
+                </AccordionTech>
 
-                <AccordionItem value="Tailwind CSS">
-                    <AccordionTrigger>
-                        <LogoLabel icon={<TailwindLogo />}>Tailwind CSS</LogoLabel>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Utility-first CSS framework for styling websites. Uses latest CSS features to outperform other styling frameworks
-                    </AccordionContent>
-                </AccordionItem>
+                <AccordionTech
+                    icon={TailwindLogo}
+                    label="Tailwind CSS"
+                >
+                    Utility-first CSS framework for styling websites. Uses latest CSS features to outperform other styling frameworks
+                </AccordionTech>
 
-                <AccordionItem value="shadcn/ui">
-                    <AccordionTrigger>
-                        <LogoLabel icon={<ShadcnUILogo />}>shadcn/ui</LogoLabel>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Collection of components built on top accessible and unstyled primitives from Radix UI. Integrated with Tailwind CSS to provide support for React Server Components
-                    </AccordionContent>
-                </AccordionItem>
+                <AccordionTech
+                    icon={ShadcnUILogo}
+                    label="shadcn/ui"
+                >
+                    Collection of components built on top accessible and unstyled primitives from Radix UI. Integrated with Tailwind CSS to provide support for React Server Components
+                </AccordionTech>
             </Accordion>
         </section>
     )
