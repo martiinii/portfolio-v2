9edb22207c88d0a0f2df36527ecd512e4a586057
Martiinii <me.martiinii@gmail.com>
2023-07-02 00:31:19 +0200

    Changed to fullstack

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter, } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-    title: 'Martin | Frontend web developer',
+    title: 'Martin | Fullstack web developer',
     description: 'Martin is mainly a front-end developer who likes to overcome challenges',
 }
 
