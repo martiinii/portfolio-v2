1d9e36f9236cbf3b82c2c6f834de2e3480c3729a
Martini <me.martiinii@gmail.com>
2023-07-02 15:22:56 +0200

    Fixed invalid properties names

--- components/icons/nextjs.tsx
+++ components/icons/nextjs.tsx
@@ -14,12 +14,12 @@ const NextLogo = forwardRef<SVGSVGElement, SVGAttributes<SVGSVGElement>>(({ clas
             </g>
             <defs>
                 <linearGradient gradientUnits="userSpaceOnUse" id=":R0:paint0_linear_408_134" x1="109" x2="144.5" y1="116.5" y2="160.5">
-                    <stop stop-color="white"></stop>
-                    <stop offset="1" stop-color="white" stop-opacity="0"></stop>
+                    <stop stopColor="white"></stop>
+                    <stop offset="1" stopColor="white" stopOpacity="0"></stop>
                 </linearGradient>
                 <linearGradient gradientUnits="userSpaceOnUse" id=":R0:paint1_linear_408_134" x1="121" x2="120.799" y1="54" y2="106.875">
-                    <stop stop-color="white"></stop>
-                    <stop offset="1" stop-color="white" stop-opacity="0"></stop>
+                    <stop stopColor="white"></stop>
+                    <stop offset="1" stopColor="white" stopOpacity="0"></stop>
                 </linearGradient>
             </defs>
         </svg>
