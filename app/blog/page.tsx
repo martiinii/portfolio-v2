3086fe7c753edcc16c650a5ef7fdc9fe3c6af11e
Martiinii <me.martiinii@gmail.com>
2023-07-02 15:16:48 +0200

    MDX blog post test

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import BlogTest from './test.mdx'
+
+export default function BlogPage() {
+    return (
+        <article className='prose prose-invert'>
+            <BlogTest />
+        </article>
+    )
+}
