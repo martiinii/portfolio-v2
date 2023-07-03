26d7ab0944f17c87d20c2ea49abd24212fcb5791
Martiinii <me.martiinii@gmail.com>
2023-07-03 14:01:15 +0200

    added sameTab property

--- components/sections/hero/heroButtonLink.tsx
+++ components/sections/hero/heroButtonLink.tsx
@@ -3,16 +3,17 @@ import { ButtonIconLink } from "../../ui/buttonIconLink"
 type HeroButtonLinkProps = {
     href: string,
     title: string,
-    icon: React.ReactNode
+    icon: React.ReactNode,
+    sameTab?: boolean
 }
-export const HeroButtonLink = ({ href, icon, title }: HeroButtonLinkProps) => {
+export const HeroButtonLink = ({ href, icon, title, sameTab }: HeroButtonLinkProps) => {
     return (
         <ButtonIconLink className="p-5 h-min" variant={'outline'}
             href={href}
             icon={icon}
             title={title}
             linkProps={{
-                target: '_blank'
+                target: sameTab ? '_self' : '_blank'
             }}
         />
 
