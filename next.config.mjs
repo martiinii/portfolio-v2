742dec33f20e975985afebad8fd4bb6cb4be1896
Martiinii <me.martiinii@gmail.com>
2023-07-04 18:23:01 +0200

    Move from firebase to GH pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/martiinii-portfolio-v2',
     // mdxRs is rust-based MDX compiler. Not recommended for production
     // experimental: {
     //     mdxRs: true
