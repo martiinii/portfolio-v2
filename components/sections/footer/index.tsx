07e0cc2a7b61d464b9a03c38e46113695516457a
Martini <me.martiinii@gmail.com>
2023-08-09 21:15:32 +0200

    Removed date

--- components/sections/footer/index.tsx
+++ components/sections/footer/index.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="text-center max-w-7xl mx-auto px-6 mt-20 mb-10 tracking-tight">
-            <span>Designed and built by Martin in 2023</span>
+            <span>Designed and built by Martin</span>
         </footer>
     )
 }
