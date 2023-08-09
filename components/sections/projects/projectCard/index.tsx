a90874cc93350daf99038a0662dd32d75ee112f3
Martini <me.martiinii@gmail.com>
2023-08-09 21:15:54 +0200

    Code organization

--- components/sections/projects/projectCard/index.tsx
+++ components/sections/projects/projectCard/index.tsx
@@ -1,58 +1,31 @@
 import { ProjectType } from "@/projects"
 import { ProjectHeader } from "./projectHeader"
-import { Badge } from "@/components/ui/badge"
-import { ButtonIconLink } from "@/components/ui/buttonIconLink"
-import { Card, CardHeader, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+import { Card } from "@/components/ui/card"
+import { ProjectContent } from "./projectContent"
 
 
-type SkillBadgeProps = {
-    skill: SkillType
-}
-const SkillBadge = ({ skill }: SkillBadgeProps) => {
-    return (
-        <Badge variant='skill'>{skill}</Badge>
-    )
-}
-
-export const ProjectCard = ({ title, date, description, links, skills }: ProjectType) => {
+export const ProjectCard = ({ title, date, shortDescription, description, img, links, skills }: ProjectType) => {
     return (
         <Card className='max-w-md md:max-w-xl w-full relative shadow-transparent shadow-xl h-fit
-        hover:bg-zinc-950/20 hover:shadow-slate-900/30 transition ease-in
+        hover:bg-zinc-950/20 hover:shadow-gray-900/30 transition ease-in
         after:h-20 after:w-px after:absolute after:-left-px after:top-3/4 after:opacity-0 after:bg-gradient-to-b after:from-transparent after:via-blue-600/80 after:to-transparent
         hover:after:top-[.2rem] hover:after:opacity-100
         after:transition-all after:duration-700 after:ease-in-out
         lg:group-hover/list:opacity-60
         lg:hover:!opacity-100
         '>
-            <CardHeader>
-                <ProjectHeader
-                    title={title}
-                    date={date}
-                />
-                <CardDescription>{description}</CardDescription>
-            </CardHeader>
-            <CardContent className='flex flex-wrap gap-2 justify-center drop-shadow-sm'>
-                {skills.map((s, i) => <SkillBadge key={i} skill={s} />)}
-            </CardContent>
-            <CardFooter className='flex-col [&>*]:w-full gap-4 h-full'>
-                {links.map(l => {
-                    const Icon = l.icon;
-                    return (
-                        <ButtonIconLink
-                            href={l.href}
-                            icon={<Icon className='scale-150' />}
-                            title={l.title}
-                            variant={'secondary'}
-                            key={l.title}
-                            linkProps={{
-                                target: '_blank'
-                            }}
-                        >
-                            {l.title}
-                        </ButtonIconLink>
-                    )
-                })}
-            </CardFooter>
+            <ProjectHeader
+                title={title}
+                img={img}
+            />
+            <ProjectContent
+                title={title}
+                shortDescription={shortDescription}
+                description={description}
+                date={date}
+                skills={skills}
+                links={links}
+            />
         </Card>
     )
 }
