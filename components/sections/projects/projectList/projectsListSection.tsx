f4126c65dba51ac2828e413dc4c5ee5c1b382189
Martini <me.martiinii@gmail.com>
2023-08-09 21:16:29 +0200

    Removed unused bg color

--- components/sections/projects/projectList/projectsListSection.tsx
+++ components/sections/projects/projectList/projectsListSection.tsx
@@ -1,10 +1,9 @@
 import { ProjectType } from "@/projects"
 import { ProjectCard } from "../projectCard"
-import { ArrowTopRightIcon, ReaderIcon, RocketIcon } from "@radix-ui/react-icons"
-import { Badge, badgeVariants } from "@/components/ui/badge"
+import { RocketIcon } from "@radix-ui/react-icons"
+import { badgeVariants } from "@/components/ui/badge"
 import { Separator } from "@/components/ui/separator"
 import clsx from "clsx"
-import { ButtonIconLink } from "@/components/ui/buttonIconLink"
 import Link from "next/link"
 
 const gridGap = "gap-8 md:gap-5"
@@ -40,11 +39,11 @@ export const ProjectListSection = ({ projects }: ProjectListSectionProps) => {
     const masonryArray = generateArrayChunks(projects, 2);
 
     return (
-        <section className='bg-slate-950 max-w-7xl m-auto p-6'>
+        <section className='max-w-7xl m-auto p-6'>
             <header className="text-center mb-8">
                 <h2 className='font-bold text-5xl mb-5'>My projects</h2>
 
-                <Link href={'/blog'} className={badgeVariants({ variant: 'outline', className: 'mb-5 space-x-2 hover:bg-slate-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background' })} >
+                <Link href={'/blog'} className={badgeVariants({ variant: 'outline', className: 'mb-5 space-x-2 hover:bg-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background' })} >
                     <span className="text-sm">Check out my blog</span>
                     <Separator orientation="vertical" className="h-5" />
                     <RocketIcon className="scale-125" />
