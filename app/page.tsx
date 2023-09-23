d95e5e6b976efd7cb27b095aafce092bf829e7ad
Martiinii <me.martiinii@gmail.com>
2023-09-23 09:23:06 +0200

    Revalidate page daily

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Hero } from '@/components/sections/hero'
 import { MyProjects } from '@/components/sections/projects/projectList'
 import { TechStack } from '@/components/sections/techstack'
 
+export const revalidate = 86400;
+
 export default function Home() {
     return (
         <>
