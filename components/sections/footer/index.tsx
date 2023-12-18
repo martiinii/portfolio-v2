a595594bc1baa5d806e4889fbfba2a6cc74afbfb
Martiinii <me.martiinii@gmail.com>
2023-12-18 16:09:36 +0100

    Add text

--- components/sections/footer/index.tsx
+++ components/sections/footer/index.tsx
@@ -1,7 +1,13 @@
+
+import { InlineLink } from "@/components/ui/inlineLink"
+
 export const Footer = () => {
     return (
-        <footer className="text-center max-w-7xl mx-auto px-6 mt-20 mb-10 tracking-tight">
-            <span>Designed and built by Martin</span>
+        <footer className="sticky top-[100vh] w-full mt-40 border-t p-6 bg-muted/20 text-lg text-center">
+            <div className="container">
+                <p className="text-2xl">Designed and built by Martin</p>
+                <p>Graphic by amazing <InlineLink href="https://www.instagram.com/madidraw.insta/" rel="noopener" target="_blank">@madidraw.insta</InlineLink></p>
+            </div>
         </footer>
     )
 }
